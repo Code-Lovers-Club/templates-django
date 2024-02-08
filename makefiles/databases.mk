343d3df0a7ab98816a91a8f9946ac24247c47187
Dharwin Perez <dhararon@gmail.com>
2024-02-07 22:12:59 -0600

    feature: Edit user email

--- makefiles/databases.mk
+++ makefiles/databases.mk
@@ -11,12 +11,8 @@ db-recreate: ## [Database] Delete and recreate postgres image
 
 .PHONY: dumpdata
 dumpdata: ## [Database] Dumpdata for fixtures
-	@(docker compose -f ${DOCKER_COMPOSE_FILE} run admin python manage.py dumpdata auth.Permission --format=json) > ./src/fixtures/auth_permissions.json
-	@(docker compose -f ${DOCKER_COMPOSE_FILE} run admin python manage.py dumpdata auth.Group --format=json) > ./src/fixtures/auth_groups.json
 	@(docker compose -f ${DOCKER_COMPOSE_FILE} run admin python manage.py dumpdata users --format=json) > ./src/fixtures/users.json
 
 .PHONY: loaddata
 loaddata: ## [Database] Load for fixtures
-	@(docker compose -f ${DOCKER_COMPOSE_FILE} run admin python manage.py loaddata --format=json fixtures/auth_permissions.json)
-	@(docker compose -f ${DOCKER_COMPOSE_FILE} run admin python manage.py loaddata --format=json fixtures/auth_groups.json)
 	@(docker compose -f ${DOCKER_COMPOSE_FILE} run admin python manage.py loaddata --format=json fixtures/users.json)

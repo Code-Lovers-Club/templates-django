4584cdfaae5df7c040c00806311d12d22121ff69
Dharwin Perez <dhararon@gmail.com>
2024-02-16 09:18:16 -0600

    feature: Add db creation, db migrate and db loaddata for full access

--- {{ cookiecutter.project_slug}}/makefiles/setup.mk
+++ {{ cookiecutter.project_slug}}/makefiles/setup.mk
@@ -2,12 +2,13 @@
 init: ## [SetUp] Setup the project configuration
 	@git init\
 	&& mv .env.example .env \
-	&& mv ./environments/admin/{{ cookiecutter.admin_environment }}/.env-django.example ./environments/admin/{{ cookiecutter.admin_environment }}/.env-django \
-	&& mv ./environments/admin/{{ cookiecutter.admin_environment }}/.env-postgres.example ./environments/admin/{{ cookiecutter.admin_environment }}/.env-postgres \
-	&& mv ./environments/api/{{ cookiecutter.api_environment }}/.env-django.example ./environments/api/{{ cookiecutter.api_environment }}/.env-django \
-	&& mv ./environments/api/{{ cookiecutter.api_environment }}/.env-postgres.example ./environments/api/{{ cookiecutter.api_environment }}/.env-postgres \
+	&& mv ./environments/admin/local/.env-django.example ./environments/admin/local/.env-django \
+	&& mv ./environments/admin/local/.env-postgres.example ./environments/admin/local/.env-postgres \
+	&& mv ./environments/api/local/.env-django.example ./environments/api/local/.env-django \
+	&& mv ./environments/api/local/.env-postgres.example ./environments/api/local/.env-postgres \
 	&& pip install -U pip poetry poetry-plugin-up pre-commit\
 	&& poetry install --no-root \
 	&& pre-commit install \
 	&& curl https://raw.githubusercontent.com/PixelFlowMX/branch-color/master/install.sh --output /tmp/install-branch-color.sh
-	/bin/bash /tmp/install-branch-color.sh
+	/bin/bash /tmp/install-branch-color.sh \
+	&& make db && make migrate && make loaddata

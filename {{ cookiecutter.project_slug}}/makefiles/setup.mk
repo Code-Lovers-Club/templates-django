f7073361b018baf4350fc3a263c1000ab2bf877c
Dharwin Perez <dhararon@gmail.com>
2024-02-16 09:21:12 -0600

    chore: Add final message into init command

--- {{ cookiecutter.project_slug}}/makefiles/setup.mk
+++ {{ cookiecutter.project_slug}}/makefiles/setup.mk
@@ -11,4 +11,5 @@ init: ## [SetUp] Setup the project configuration
 	&& pre-commit install \
 	&& curl https://raw.githubusercontent.com/PixelFlowMX/branch-color/master/install.sh --output /tmp/install-branch-color.sh
 	/bin/bash /tmp/install-branch-color.sh \
-	&& make db && make migrate && make loaddata
+	&& make db && make migrate && make loaddata \
+	&& clear && echo "Your email for django admin access is: {{ cookiecutter.email }} and password: admin123"

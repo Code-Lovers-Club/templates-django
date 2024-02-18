d1691ed4969ff000b7590a288525cfbbcfbf401f
Dharwin Perez <dharwin@getfairplay.com>
2024-02-17 23:50:41 -0600

    chore: Remove drf-spectacular configuration

--- {{ cookiecutter.project_slug}}/src/settings/api/rest_framework.py
+++ {{ cookiecutter.project_slug}}/src/settings/api/rest_framework.py
@@ -9,7 +9,6 @@ class BaseRestFrameworkConfig:
             "DEFAULT_PERMISSION_CLASSES": [
                 "rest_framework.permissions.IsAuthenticated",
             ],
-            "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
         }
 
 

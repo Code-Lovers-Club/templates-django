961f97cd2ff05b7422608400d4f33992fe011230
Dharwin Perez <dharwin@getfairplay.com>
2024-02-17 23:45:09 -0600

    chore: Delete drf-spectacular and remove endpoints

--- {{ cookiecutter.project_slug}}/src/urls/api.py
+++ {{ cookiecutter.project_slug}}/src/urls/api.py
@@ -1,17 +1,6 @@
 # Third Party Stuff
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 from urls.common import urlpatterns as base_urlpatterns
 
-urlpatterns = [
-    *base_urlpatterns,
-    # OpenApi configuration
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # Redoc UI:
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
-]
+urlpatterns = [*base_urlpatterns]
